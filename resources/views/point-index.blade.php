1a422d51fa2f1d0982be2de08561395ff365ff46
ittz-soran <soranhawrami@icloude.com>
2024-08-04 16:17:32 +0300

    Update

--- resources/views/point-index.blade.php
+++ resources/views/point-index.blade.php
@@ -1,8 +1,6 @@
 <div>
     <div class="container">
 
-        
-
         <livewire:CraetePoint />
         <input type="text" wire:model.live.debounce.250ms="search">
         @if (session('status'))
