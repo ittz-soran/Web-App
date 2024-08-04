f6888986083de42ca0e7364baa608b05a8b0863c
ittz-soran <soranhawrami@icloude.com>
2024-08-04 16:15:07 +0300

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,36 +1,81 @@
-<div class=" container">
-    Nothing in the world is as soft and yielding as water.
-    <br>
-    {{$number}}
-    <br>
-        <div class="" style="">
-        <button class="btn btn-primary" wire:click="increment">+</button>
-        <button wire:click="decrement">-</button>
-        <br>
+<div>
+    <div class="container">
+
+        
+        <div>
+            <livewire:CraetePoint />
         </div>
 
-
-    <input type="text" wire:model.live="name" placeholder="Live">
-    <br>
-    <input type="text" wire:model.lazy="name" placeholder="Lazy">
-    <br>
-    <input type="text" wire:model.live.debounce.500ms="name" placeholder="500ms">
-    <br>
-    {{$name}}
-
-    <br>
-    @foreach ($points as $Point)
-    <div class="grid text-center">
-        <div class="card m-2" style="width: 18rem;">
-            <div class="card-body">
-            <h5 class="card-title">{{$Point->name}}</h5>
-            <h6 class="card-subtitle mb-2 text-body-secondary">{{$Point->address}}</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
-            </div>
+        
+        <input type="text" wire:model.live.debounce.250ms="search">
+        
         </div>
+            @if (session('status'))
+                <div class="alert alert-danger">
+                    {{ session('status') }}
+                </div>
+            @endif
+        <div class=" d-flex flex-wrap justify-content-start">
+            @forelse ($points as $point)
+                <div class="">
+                    <div class="card m-2" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $point->name }}</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">{{ $point->address }}</h6>
+                            <button wire:click="Delete({{$point->id}})" class="btn btn-danger btn-sm">Delete</button>
+                            <button wire:click="findPoint({{$point->id}})" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#editPoint{{$point->id}}">Edit</button>
+                            <a wire:click="findPoint({{$point->id}})" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#editPoint{{$point->id}}">View</a>
+
+                        
+                            
+                            <!-- Modal -->
+                            <div wire:ignore.self class="modal fade" id="editPoint{{$point->id}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Point Update</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    
+                                    <div class="modal-body">
+                                        <form wire:submit.prevent="update({{$point->id}})" >
+                                            <div class="mb-3">
+                                            <label class="form-label">Name</label>
+                                            <input type="name" class="form-control @error('name') border-danger @enderror" wire:model.defer="name">
+                                            @error('name') 
+                                            <span class="text-danger">{{ $message }}</span>
+                                            @enderror
+                                            </div>
+                                            <div class="mb-3">
+                                            <label class="form-label">Address</label>
+                                            <input type="text" class="form-control @error('address') border-danger @enderror" wire:model.defer="address">
+                                            @error('address') 
+                                            <span class="text-danger">{{ $message }}</span>
+                                            @enderror
+                                            </div>
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            <button type="submit" class="btn btn-primary">Update</button>
+                                            
+                                        </form>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            @empty
+                    No data found.
+            @endforelse
+        </div>
+
+        @if ($paginate < $points->total())
+        <div class="d-flex flex-wrap justify-content-center my-4">
+            <button wire:click="loadMore">Load More</button>
+        </div>
+        @endif
+        
+    
     </div>
-    @endforeach
-   
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Controllers\MainDashboard;
 use App\Controllers\Welcome;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/',Welcome::class)->name('/');
+// Route::get('/',Welcome::class)->name('/');
+
